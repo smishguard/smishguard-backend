83144124478ba28b875872b28a8578fd82e9916b
Esteban Pedraza <estemanps99@gmail.com>
2024-08-04 23:12:16 -0500

    dejando api base mas limpia

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, jsonify, request
-from info_test import info_test
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
 
 @app.route('/')
 def hello_world():
@@ -11,7 +9,3 @@ def hello_world():
 @app.route("/ping")
 def ping():
     return jsonify({"message": "pong"})
-
-
-if __name__ == '__main__':
-    app.run(debug = True, port=4000)
